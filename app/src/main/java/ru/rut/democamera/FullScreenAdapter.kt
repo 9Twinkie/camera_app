2219bc18c5df963e5c0d39ce0ba23a48eb7acdc4
d50776310@gmail.com <d50776310@gmail.com>
2024-12-20 19:59:46 +0300

    m

---
+++ app/src/main/java/ru/rut/democamera/FullScreenAdapter.kt
@@ -0,0 +1,46 @@
+package ru.rut.democamera
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.MediaController
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ru.rut.democamera.databinding.ItemFullscreenMediaBinding
+import java.io.File
+import java.util.*
+
+class FullScreenAdapter(private var files: List<File>) :
+    RecyclerView.Adapter<FullScreenAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: ItemFullscreenMediaBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemFullscreenMediaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount() = files.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val file = files[position]
+        val extension = file.extension.lowercase(Locale.getDefault())
+
+        if (extension in listOf("mp4", "mov")) {
+            holder.binding.imageView.visibility = android.view.View.GONE
+            holder.binding.videoView.visibility = android.view.View.VISIBLE
+            holder.binding.videoView.setVideoURI(Uri.fromFile(file))
+            val mediaController = MediaController(holder.binding.root.context)
+            mediaController.setAnchorView(holder.binding.videoView)
+            holder.binding.videoView.setMediaController(mediaController)
+            holder.binding.videoView.start()
+        } else {
+            holder.binding.videoView.visibility = android.view.View.GONE
+            holder.binding.imageView.visibility = android.view.View.VISIBLE
+            Glide.with(holder.binding.root)
+                .load(file)
+                .fitCenter()
+                .into(holder.binding.imageView)
+        }
+    }
+}
---
+++ app/src/main/java/ru/rut/democamera/MediaGridAdapter.kt
@@ -0,0 +1,54 @@
+package ru.rut.democamera
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ru.rut.democamera.databinding.ItemMediaGridBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MediaGridAdapter(
+    private val files: List<File>,
+    private val listener: OnItemClickListener
+) : RecyclerView.Adapter<MediaGridAdapter.ViewHolder>() {
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    class ViewHolder(val binding: ItemMediaGridBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemMediaGridBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount() = files.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val file = files[position]
+
+        Glide.with(holder.binding.root)
+            .load(file)
+            .centerCrop()
+            .into(holder.binding.mediaThumbnail)
+
+        val extension = file.extension.lowercase(Locale.getDefault())
+        val type = when (extension) {
+            in listOf("jpg", "jpeg", "png") -> "Photo"
+            in listOf("mp4", "mov") -> "Video"
+            else -> "Unknown"
+        }
+        holder.binding.mediaType.text = type
+
+        val sdf = SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.getDefault())
+        val dateStr = sdf.format(Date(file.lastModified()))
+        holder.binding.mediaDate.text = dateStr
+
+        holder.itemView.setOnClickListener {
+            listener.onItemClick(position)
+        }
+    }
+}
