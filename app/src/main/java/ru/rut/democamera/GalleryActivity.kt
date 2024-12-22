5f3632214ac0a1e498ca87fbadcda0c95cda6312
d50776310@gmail.com <d50776310@gmail.com>
2024-12-22 15:44:15 +0300

    m

--- app/src/main/java/ru/rut/democamera/GalleryActivity.kt
+++ app/src/main/java/ru/rut/democamera/GalleryActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import ru.rut.democamera.databinding.ActivityGalleryBinding
 import java.io.File
-class GalleryActivity : AppCompatActivity(), MediaGridAdapter.OnItemClickListener {
+class GalleryActivity : AppCompatActivity(), MediaGridAdapter.OnItemClickListener, NavBarFragment.NavBarListener {
     private lateinit var binding: ActivityGalleryBinding
     private lateinit var mediaFiles: List<File>
 
@@ -23,15 +23,28 @@ class GalleryActivity : AppCompatActivity(), MediaGridAdapter.OnItemClickListene
         binding.recyclerView.layoutManager = GridLayoutManager(this, 3)
         val adapter = MediaGridAdapter(mediaFiles, this)
         binding.recyclerView.adapter = adapter
-
-        binding.createNewContentBtn.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
+        supportFragmentManager.beginTransaction()
+            .replace(binding.navbarContainer.id, NavBarFragment(this))
+            .commit()
     }
 
     override fun onItemClick(position: Int) {
         val intent = Intent(this, FullScreenActivity::class.java)
         intent.putExtra("current_index", position)
+        startActivity(intent)
+    }
+
+    override fun onGallerySelected() {
+
+    }
+
+    override fun onPhotoModeSelected() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onVideoModeSelected() {
+        val intent = Intent(this, VideoActivity::class.java)
         startActivity(intent)
     }
 }
